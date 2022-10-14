a6e9296aaca9021ebc693313a98d62a163537e49
Grzegorz Zioło <grisha@Grzegorzs-MacBook-Air.local>
2022-10-14 14:07:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int AmountOfBricks = 70
+        int ContainerCapacity = 8;
+        int Containers = AmountOfBricks/ContainerCapacity;
+        
+
+    }
+}
